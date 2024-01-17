987e5908d68350201a8485fec8daeac368b0d29e
jayparikh1998 <67886105+jayparikh1998@users.noreply.github.com>
2024-01-16 19:39:46 -0500

    Finished PyPoll assignment

--- python_challenge/python_challenge.py
+++ python_challenge/python_challenge.py
@@ -35,3 +35,21 @@ print(f"Greatest decrease in Profit/Losses over the entire period ${min_change} 
 
 #Reading in the CSV file
 poll_data = pd.read_csv("C:/Users/jaypa/OneDrive/Documents/GitHub/UofT Bootcamp/python_challenge/PyPoll/Resources/election_data.csv")
+
+#The total number of votes cast
+total_votes = len(poll_data)
+
+#All candidates, percentage and total number of votes each candidate won
+# Group by 'Candidate' and count
+candidate_count = poll_data.groupby('Candidate').size()
+total_count = len(poll_data)
+proportions = (candidate_count / total_count) * 100
+result = pd.DataFrame({'Count': candidate_count, 'Proportion (%)': proportions})
+
+#The winner of the election based on popular vote
+popular_vote = result["Count"].max()
+winner = result.index[result['Count'] == popular_vote].tolist()[0]
+
+print(f"The total number of votes casted in the dataset is {total_votes}")
+print(result)
+print(f"The winner of the election based on populat vote is {winner}")
